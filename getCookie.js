70ca0cbb46b721be2ec848038875f1e086759210
notanewbie <computerchipcity@aol.com>
2016-06-03 17:22:31 -0400

    Create getCookie.js

---
+++ getCookie.js
@@ -0,0 +1,15 @@
+function getCookie(Name) {
+	//document.cookie="radio_central=It works, woo-hoo!";
+	cookies = document.cookie.split(';');
+	for (i = 0; i < cookies.length; i++) {
+		//document.write(cookies[i] + "<br />");
+		isthere = cookies[i].indexOf(Name);
+		if (isthere != -1) {
+			//document.write("Aha! I have found it! At " + cookies[i] + "<br />");
+			right_number = i;
+			cookie_u_want = cookies[right_number].split("=");
+			cookie_content = cookie_u_want[1];
+			return cookie_content;
+		}
+	}
+}
